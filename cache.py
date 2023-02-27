e4fed1c7e89b92a849f61610d13036240ae44933
IllCher <ilya.chernenko.2012@gmail.com>
2023-02-27 19:12:40 +0300

    separated test and class

---
+++ cache.py
@@ -0,0 +1,25 @@
+class LRUCache:
+    def __init__(self, capacity: int = 10) -> None:
+        self.capacity = capacity
+        self.cache = {}
+
+    def get(self, key: str) -> str:
+        if key in self.cache:
+            value = self.cache[key]
+            del self.cache[key]
+            self.cache[key] = value
+            return value
+        else:
+            return ""
+
+    def set(self, key: str, value: str) -> None:
+        if key in self.cache:
+            del self.cache[key]
+        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            fc = next(iter(self.cache))
+            self.cache.pop(fc)
+
+    def rem(self, key: str) -> None:
+        if key in self.cache:
+            del self.cache[key]
--- t1.py
+++ t1.py
@@ -1,28 +1,4 @@
-class LRUCache:
-    def __init__(self, capacity: int = 10) -> None:
-        self.capacity = capacity
-        self.cache = {}
-
-    def get(self, key: str) -> str:
-        if key in self.cache:
-            value = self.cache[key]
-            del self.cache[key]
-            self.cache[key] = value
-            return value
-        else:
-            return ""
-
-    def set(self, key: str, value: str) -> None:
-        if key in self.cache:
-            del self.cache[key]
-        self.cache[key] = value
-        if len(self.cache) > self.capacity:
-            self.cache.popitem(last=False)
-
-    def rem(self, key: str) -> None:
-        if key in self.cache:
-            del self.cache[key]
-            
+from cache import LRUCache          
 
 cache = LRUCache(100)
 cache.set('Jesse', 'Pinkman')
