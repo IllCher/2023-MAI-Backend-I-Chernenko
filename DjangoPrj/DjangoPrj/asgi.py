b347e6a54b39ec0454bc693d7d739e6303e0b40f
IllCher <ilya.chernenko.2012@gmail.com>
2023-05-01 15:19:19 +0300

    back

--- DjangoPrj/DjangoPrj/asgi.py
+++ DjangoPrj/DjangoPrj/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for djangoprj project.
+ASGI config for DjangoPrj project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoprj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoPrj.settings')
 
 application = get_asgi_application()
--- DjangoPrj/DjangoPrj/wsgi.py
+++ DjangoPrj/DjangoPrj/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for djangoprj project.
+WSGI config for DjangoPrj project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoprj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoPrj.settings')
 
 application = get_wsgi_application()
